45e243a0c74bd6ff1ffc8cbe532f4700e20040a4
OyolloBrian <oyollobrian@gmail.com>
2023-02-15 22:20:11 +0300

    add documentation

--- listing.py
+++ listing.py
@@ -18,42 +18,56 @@ class CarScrapper:
         
     
     def get_live_auction_urls(self): 
+        """
+        This method fetches the auction URLs of all the ongoing auctions on the "https://carsandbids.com" website. 
+        
+        Args:
+            self: Instance of the CarsandBidsScraper class.
+            
+        Returns:
+            A tuple consisting of three elements:
+            - The list of all auction URLs,
+            - The length of the list of auction URLs, and
+            - The total elapsed time taken to fetch the auction URLs.
+        """
         start_time = time.time()
         
         self.driver.get(self.url)
-        # time.sleep(10)
-        vehicle_urls = WebDriverWait(self.driver, 20).until(
-            EC.presence_of_all_elements_located((By.XPATH, "//div[@class='auction-title']/a")))
+        vehicle_urls = WebDriverWait(self.driver, 20).until(EC.presence_of_all_elements_located((By.XPATH, "//div[@class='auction-title']/a")))
         urls = [url.get_attribute('href') for url in vehicle_urls] # might contain duplicates. 
         final_live_auction_urls = list(set(urls)) # convert to set to remove duplicates if any
         
         elapsed_time = time.time() - start_time
         
-        # print(final_live_auction_urls)
-        # print(len(final_live_auction_urls))
-        # print(f"{elapsed_time} seconds")
-        
         return final_live_auction_urls,len(final_live_auction_urls), elapsed_time, 
 
 
     def get_past_auctions_urls(self):
+        """
+        Navigates to the "Past Auctions" page on carsandbids.com and returns a list of URLs for each past auction.
+    
+        Returns:
+        - final_past_auction_urls: a list of unique URLs for each past auction
+        - num_urls: the number of URLs in the list
+        - elapsed_time: the time elapsed while scraping the URLs
+        
+        Raises:
+        - TimeoutException: if the page or element could not be found within the allotted time (20 seconds)
+        - Exception: if there was an error during the scraping process
+        """
         start_time = time.time()
-        
-        
+            
         final_past_auction_urls = []
         page_num=1
-        while True:
-            
+        while True:  
             try:
                 self.driver.get(f"https://carsandbids.com/past-auctions/?page={page_num}")
                 print(f"Scrapping page {page_num} ...")
-                past_auctions_urls = WebDriverWait(self.driver,20).until(
-                    EC.presence_of_all_elements_located((By.XPATH, "//div[@class='auction-title']/a")))
+                past_auctions_urls = WebDriverWait(self.driver,20).until(EC.presence_of_all_elements_located((By.XPATH, "//div[@class='auction-title']/a")))
                 page_urls = [url.get_attribute('href') for url in past_auctions_urls] # might contain duplicates. 
                 for final_url in list(set(page_urls)):  # convert to set to remove duplicates
                     final_past_auction_urls.append(final_url)
                    
-                
                 next_page_btn = WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//li[@class='arrow next']/button[@class='btn rb btn-link']")))
                 time.sleep(5)
                 self.driver.execute_script("arguments[0].click();", next_page_btn)
@@ -64,8 +78,10 @@ class CarScrapper:
     
         elapsed_time = time.time()- start_time
         
-        print(final_past_auction_urls)
-        print(f"{len(final_past_auction_urls)} urls scrapped in {elapsed_time}")
+        # print(final_past_auction_urls)
+        # print(f"{len(final_past_auction_urls)} urls scrapped in {elapsed_time}")
+        
+        return final_past_auction_urls, len(final_past_auction_urls), elapsed_time
         
 
 
