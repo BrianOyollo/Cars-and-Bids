ee0a8b3754c7352565ab9f8b802bbe18f9cfaa55
OyolloBrian <oyollobrian@gmail.com>
2023-02-20 22:11:18 +0300

    get auction-details

--- listing.py
+++ listing.py
@@ -79,14 +79,11 @@ class CarScrapper:
         elapsed_time = time.time()- start_time
         
         
-        return final_past_auction_urls, len(final_past_auction_urls), elapsed_time
-        
-
-    def dump_past_urls(self,past_urls):
         with open('auction_urls.txt', 'w') as obj:
             print(f"Writing urls to file...")
-            for url in past_urls:
+            for url in past_auctions_urls:
                 obj.writelines(f"{url}\n")
+                
             
     def update_past_auction_urls(self):
         with open('auction_urls.txt', 'r') as obj:
@@ -126,9 +123,207 @@ class CarScrapper:
             for url in daily_urls:
                 obj.writelines(f"{url}\n")
         
-     
+    def get_auction_details(self):
+                
+        def load_auction_page(url):
+            try:
+                self.driver.get(url.strip('\n'))
+            except Exception as e:
+                print(f"Oops, something went wrong while loading {url}")
+                print(e)
+                
+            return self.driver
+                
+        def get_auction_title(driver):
+            try:    
+                auction_title = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//div[@class='auction-title ']/h1"))).text
+            except Exception as e:
+                print("Error getting auction title")
+                auction_title = None
+
+            return auction_title
+
+        def get_auction_subtitle(driver):
+            try:    
+                auction_subtitle = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//div[@class='d-md-flex justify-content-between flex-wrap']/h2"))).text
+            except Exception as e:
+                print("Error getting auction sub-title")
+                auction_subtitle = None
+            
+            return auction_subtitle
+                
+        def get_quick_facts(driver):
+            quick_facts = {}
+            try:
+                auction_quick_facts_dt = WebDriverWait(driver, 20).until(EC.presence_of_all_elements_located((By.XPATH, "//div[@class='quick-facts']/dl/dt")))
+                auction_quick_facts_dd = WebDriverWait(driver, 20).until(EC.presence_of_all_elements_located((By.XPATH, "//div[@class='quick-facts']/dl/dd")))
+            except Exception as e:
+                print("Error getting auction quick-facts")
+                auction_quick_facts_dt = []
+                auction_quick_facts_dd = []    
+                
+            for dt,dd in zip(auction_quick_facts_dt, auction_quick_facts_dd):
+                quick_facts[dt.text]=dd.text
+                
+            return quick_facts
+
+        def get_dougs_take(driver):
+            try:
+                dougs_take = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//div[@class='detail-section dougs-take']/div[@class='detail-body']/p"))).text
+            except Exception as e:
+                print("Error getting Doug's take")
+                dougs_take = None
+            
+            return dougs_take
+                
+        def get_auction_highlights(driver):
+            try:
+                auction_highlights = WebDriverWait(driver,20).until(EC.presence_of_all_elements_located((By.XPATH, "//div[@class='detail-section detail-highlights']/div[@class='detail-body']/ul/li")))
+                highlights = [highlight.text for highlight in auction_highlights]
+            except Exception as e:
+                print("Error getting auction's highlights")
+                highlights = []
+                
+            return highlights
+        
+        def get_auction_equiment(driver):
+            try:
+                auction_equipment = WebDriverWait(driver,20).until(EC.presence_of_all_elements_located((By.XPATH, "//div[@class='detail-section detail-equipment']/div[@class='detail-body']/ul/li")))
+                equipment = [equipment.text for equipment in auction_equipment]
+            except Exception as e:
+                print("Error getting auction's equipment")
+                equipment = []
+                
+            return equipment
+
+        def get_auction_modifications(driver):
+            try:
+                auction_modifications = WebDriverWait(driver,20).until(EC.presence_of_all_elements_located((By.XPATH, "//div[@class='detail-section detail-modifications']/div[@class='detail-body']/ul/li")))
+                modifications = [modification.text for modification in auction_modifications]
+            except Exception as e:
+                print("Error getting auction's modifications")
+                modifications = []
+                
+            return modifications
+
+        def get_known_flaws(driver):
+            try:
+                auction_known_flaws = WebDriverWait(driver,20).until(EC.presence_of_all_elements_located((By.XPATH, "//div[@class='detail-section detail-known_flaws']/div[@class='detail-body']/ul/li")))
+                known_flaws = [flaw.text for flaw in auction_known_flaws]
+            except Exception as e:
+                print("Error getting auction's known flaws")
+                known_flaws = []
+                
+            return known_flaws
+
+        def get_service_history(driver):
+            try:
+                auction_service_history = WebDriverWait(driver,20).until(EC.presence_of_all_elements_located((By.XPATH, "//div[@class='detail-section detail-recent_service_history']/div[@class='detail-body']/ul/li")))
+                services = [service_history.text for service_history in auction_service_history]
+            except Exception as e:
+                print("Error getting auction's service history")
+                services = []
+                
+            return services
+
+        def get_included_items(driver):
+            try:
+                other_included_items = WebDriverWait(driver,20).until(EC.presence_of_all_elements_located((By.XPATH, "//div[@class='detail-section detail-other_items']/div[@class='detail-body']/ul/li")))
+                included_items = [item.text for item in other_included_items]
+            except Exception as e:
+                print("Error getting auction's included items")
+                included_items = []
+                
+            return included_items    
+
+        def get_ownership_hostory(driver):
+            try:
+                ownership_history = WebDriverWait(driver,20).until(EC.presence_of_element_located((By.XPATH, "//div[@class='detail-section detail-ownership_history']/div[@class='detail-body']/p")))
+                ownership_history = ownership_history.text
+            except Exception as e:
+                print("Error getting auction's ownership history")
+                ownership_history = None
+            
+            return ownership_history
+
+        def get_auction_stats(driver):
+            auction_stats = {}
+            try:
+                reserve_status = WebDriverWait(driver,20).until(EC.presence_of_element_located((By.XPATH, "//div[@class='auction-subheading ']/h3/span"))).text
+                auction_status = WebDriverWait(driver,20).until(EC.presence_of_element_located((By.XPATH, "//div[@class='current-bid ended d-flex flex-column flex-shrink-1 ']/div[@class='d-flex bidder']/h4"))).text
+                highest_bid_value = WebDriverWait(driver,20).until(EC.presence_of_element_located((By.XPATH, "//span[@class='bid-value']"))).text
+                auction_date = WebDriverWait(driver,20).until(EC.presence_of_element_located((By.XPATH, "//ul[@class='stats']/li/div[@class='td end-icon']"))).text
+                bid_count = WebDriverWait(driver,20).until(EC.presence_of_element_located((By.XPATH, "//ul[@class='stats']/li/div[@class='td bid-icon']"))).text
+                view_count = WebDriverWait(driver,20).until(EC.presence_of_element_located((By.XPATH, "//ul[@class='stats']/li/div[@class='td views-icon']"))).text
+                bids = WebDriverWait(driver, 20).until(EC.presence_of_all_elements_located((By.XPATH, "//div[@class='content']/dl[@class='placed-bid ']/dd[@class='bid-value']")))
+                bid_list = [bid.text for bid in bids]
+            except Exception as e:
+                print("Error getting auction's stats")
+                reserve_status = None
+                auction_status = None
+                highest_bid_value = None
+                auction_date = None
+                bid_count = None
+                view_count = None
+                bid_list = []
+                
+            auction_stats = {
+                'reserve_status':reserve_status,
+                'auction_status':auction_status,
+                'highest_bid_value':highest_bid_value,
+                'auction_date':auction_date,
+                'view_count':view_count,
+                'bid_count':bid_count,
+                'bids':bid_list,
+            }
+            
+            return auction_stats    
+            
+        
+        with open('auction_urls.txt', 'r') as obj:
+            print("Reading auction urls...")
+            urls = obj.readlines()
+        
+        auction_details = {}
+        
+        for url in urls[:5]:
+            print(f"Scrapping {url}")
+        
+            driver = load_auction_page(url.strip('\n'))
+            auction_title = get_auction_title(driver)
+            auction_subtitle = get_auction_subtitle(driver)
+            auction_quick_facts = get_quick_facts(driver)
+            dougs_take = get_dougs_take(driver)
+            auction_highlights = get_auction_highlights(driver)
+            auction_equipment = get_auction_equiment(driver)
+            auction_modifications = get_auction_modifications(driver)
+            auction_known_flaws = get_known_flaws(driver)
+            auction_services = get_service_history(driver)
+            auction_included_items = get_included_items(driver)
+            auction_ownership_history = get_ownership_hostory(driver)
+            auction_stats = get_auction_stats(driver)
+            
+            auction_details[url.strip('\n')] = {  
+                'auction_title':auction_title,
+                'auction_subtitle':auction_subtitle,
+                'auction_quick_facts':auction_quick_facts,
+                "dougs_take":dougs_take,
+                'auction_highlights':auction_highlights,
+                'auction_equipment':auction_equipment,
+                'modifications':auction_modifications,
+                'known_flaws':auction_known_flaws,
+                'services':auction_services,
+                'included_items':auction_included_items,
+                'ownership_history':auction_ownership_history,
+                'auction_stats':auction_stats
+            }
+            
+            
+        with open('auction_details.json', 'w') as obj:
+            json.dump(auction_details, obj, indent=4)
             
 scrapper = CarScrapper("https://carsandbids.com/")
-# scrapper.dump_past_urls(scrapper.get_past_auctions_urls()[0])
+
 scrapper.update_past_auction_urls()
+
 scrapper.driver.quit()
