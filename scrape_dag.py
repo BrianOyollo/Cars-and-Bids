15124e5130c105bd89bdbac28f2af3aa1086fdb6
OyolloBrian <oyollobrian@gmail.com>
2023-03-17 17:34:31 +0300

    fix: update urls list

--- scrape_dag.py
+++ scrape_dag.py
@@ -1,151 +1,82 @@
+import airflow
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
+from datetime import datetime,timedelta
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-from bs4 import BeautifulSoup
-import time
-from datetime import datetime
 import json
+import time
+import sys
 
+# sys.path.append('/home/brian_oyollo/Documents/projects/demuro/Cars-and-Bids/')
+# from listing import CarScraper
 
+# car_scraper = CarScraper("https://carsandbids.com/")
 
-class CarScraper:
-    def __init__(self,url):
-        self.url = url
-        options = Options()
-        options.add_argument('--headless')
-        self.driver = webdriver.Firefox(options=options) 
-        
-    # get urls of live auctions
-    def get_live_auction_urls(self): 
-        """
-        This method fetches the auction URLs of all the ongoing auctions on the "https://carsandbids.com" website. 
-        
-        Args:
-            self: Instance of the CarsandBidsScraper class.
+options = Options()
+options.add_argument("--headless")
+driver = webdriver.Firefox(options=options) 
+
+def update_past_auction_urls(**context):
+    urls_file_path = context['templates_dict']['urls_file_path']
+    
+    with open('auction_urls.txt', 'r') as obj:
+        print("Reading previous URLs...")
+        old_urls = obj.readlines()
+
+    daily_urls = []
+    
+    page=1
+    while True and page <= 2:
+        try:
+            driver.get(f"https://carsandbids.com/past-auctions/?page={page}")
+            print(f"scraping page {page}...")
+            vehicle_urls = WebDriverWait(driver, 20).until(
+                    EC.presence_of_all_elements_located((By.XPATH, "//div[@class='auction-title']/a")))
             
-        Returns:
-            A tuple consisting of three elements:
-            - The list of all auction URLs,
-            - The length of the list of auction URLs, and
-            - The total elapsed time taken to fetch the auction URLs.
-        """
-        start_time = time.time()
-        
-        self.driver.get(self.url)
-        vehicle_urls = WebDriverWait(self.driver, 20).until(EC.presence_of_all_elements_located((By.XPATH, "//div[@class='auction-title']/a")))
-        urls = [url.get_attribute('href') for url in vehicle_urls] # might contain duplicates. 
-        final_live_auction_urls = list(set(urls)) # convert to set to remove duplicates if any
-        
-        elapsed_time = time.time() - start_time
-        
-        return final_live_auction_urls,len(final_live_auction_urls), elapsed_time, 
-
-    # get urls of all auctions (from Nov. 2020)
-    def get_past_auctions_urls(self):
-        
-        """
-        Navigates to the "Past Auctions" page on carsandbids.com and returns a list of URLs for each past auction.
-    
-        Returns:
-        - final_past_auction_urls: a list of unique URLs for each past auction
-        - num_urls: the number of URLs in the list
-        - elapsed_time: the time elapsed while scraping the URLs
-        
-        Raises:
-        - TimeoutException: if the page or element could not be found within the allotted time (20 seconds)
-        - Exception: if there was an error during the scraping process
-        """
-        start_time = time.time()
+            urls = [url.get_attribute('href') for url in vehicle_urls]
+            for final_url in urls:
+                daily_urls.append(final_url)
+                
+            next_page_btn = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//li[@class='arrow next']/button[@class='btn rb btn-link']")))
             
-        final_past_auction_urls = []
-        page_num=1
-        while True:  
-            try:
-                self.driver.get(f"https://carsandbids.com/past-auctions/?page={page_num}")
-                print(f"scraping page {page_num}...")
-                past_auctions_urls = WebDriverWait(self.driver,20).until(EC.presence_of_all_elements_located((By.XPATH, "//div[@class='auction-title']/a")))
-                page_urls = [url.get_attribute('href') for url in past_auctions_urls]
-                for final_url in page_urls:
-                    final_past_auction_urls.append(final_url)
-                   
-                next_page_btn = WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//li[@class='arrow next']/button[@class='btn rb btn-link']")))
-                time.sleep(5)
-                self.driver.execute_script("arguments[0].click();", next_page_btn)
-                page_num+=1
-            except:
-                print("Looks like you got all the auctions.")
-                break
+            time.sleep(5)
+            driver.execute_script("arguments[0].click();", next_page_btn)
+            page+=1
+        except Exception as e:
+            print(e)
+            break
     
-        elapsed_time = time.time()- start_time
-        print(f"{page_num} pages scrapped in {round(elapsed_time,2)}")
-        
-        
-        with open('auction_urls.txt', 'w') as obj:
-            print(f"Writing urls to file...")
-            for url in final_past_auction_urls:
-                obj.writelines(f"{url}\n")
-                
-            #   
     
-    # save urls of new auctions & update the overall list of auction URLs
-    def update_past_auction_urls(self):
-
-        with open('auction_urls.txt', 'r') as obj:
-            print("Reading previous URLs...")
-            old_urls = obj.readlines()
-
-        daily_urls = []
-        
-        page=1
-        while True and page <= 7:
-            try:
-                self.driver.get(f"https://carsandbids.com/past-auctions/?page={page}")
-                print(f"scraping page {page}...")
-                vehicle_urls = WebDriverWait(self.driver, 20).until(
-                        EC.presence_of_all_elements_located((By.XPATH, "//div[@class='auction-title']/a")))
-                
-                urls = [url.get_attribute('href') for url in vehicle_urls]
-                for final_url in urls:
-                    daily_urls.append(final_url)
-                    
-                next_page_btn = WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//li[@class='arrow next']/button[@class='btn rb btn-link']")))
-                
-                time.sleep(5)
-                self.driver.execute_script("arguments[0].click();", next_page_btn)
-                page+=1
-            except Exception as e:
-                print(e)
-                break
-        
-        
-        with open(f'daily urls/{datetime.today().date()}.txt','w') as file:
+    with open(f'daily urls/{datetime.today().date()}.txt','w') as file:
             print("Saving daily auction urls...")
             for url in daily_urls:
                 if f"{url}\n" not in old_urls:
                     file.writelines(f"{url}\n")
              
-        for index, url in enumerate(daily_urls):
-            if f"{url}\n" not in old_urls:
-                old_urls.insert(index,url.strip())        
+    for index, url in enumerate(daily_urls):
+        if f"{url}\n" not in old_urls:
+            old_urls.insert(index,url.strip())        
 
-        with open('auction_urls.txt','w') as obj:
-            print('Updating URLs...')
-            for url in old_urls:
-                obj.writelines(f"{url.strip()}\n")
-             
-    # scrape auction details
-    def scrape_auction_details(self,urls):
+    with open('auction_urls_copy.txt','w') as obj:
+        print('Updating URLs...')
+        for url in old_urls:
+            obj.writelines(f"{url.strip()}\n")
+                
+def scrape_auction_details(urls):
                 
         def load_auction_page(url):
             try:
-                self.driver.get(url.strip('\n'))
+                driver.get(url.strip('\n'))
             except Exception as e:
                 print(f"Oops, something went wrong while loading {url}")
                 print(e)
                 
-            return self.driver
+            return driver
                 
         def get_auction_title(driver):
             try:    
@@ -352,37 +283,50 @@ class CarScraper:
             
         return auction_details
 
-    # scrape and save auction details in chunks
-    def scrape_dump_in_chunks(self,chunk_size):
-        with open('auction_urls.txt', 'r') as obj:
-            print("Reading auction urls...")
-            auction_urls = obj.readlines()[9558:]
+                
+def daily_scraper(**context):
+    auctions_file_path = context['templates_dict']['auctions_file_path']
+    urls_file_path = context['templates_dict']['urls_file_path']
+    
+    with open(f"daily urls/{urls_file_path}", 'r') as file:
+        auction_urls = file.readlines()
+        auction_data = scrape_auction_details(auction_urls[:5])
         
-        for index in range(0, len(auction_urls),chunk_size):
-            batch_urls = auction_urls[index:index+chunk_size]
-            batch_name = f"{index+1}-{(index+chunk_size)}_1"
-            auctions = self.scrape_auction_details(batch_urls)        
-            with open(f"auctions_c500s_{batch_name}.json", 'w') as obj:
-                json.dump(auctions, obj, indent=4)
+    with open (f"daily auctions/{auctions_file_path}", 'w') as file:
+        json.dump(auction_data, file, indent=4)
 
-
-    def daily_scraper(self):
-        
-        with open(f"daily urls/{datetime.today().date()}", 'r') as file:
-            auction_urls = file.readlines()
-            auction_data = self.scrape_auction_details(auction_urls[:5])
+with DAG(
+    'cars_abd_bids_v4',
+    default_args={
+         'owner':"BrianOyollo",
+         'email':'oyollobrian@gmail.com',
+        "email_on_failure": True,
+        "email_on_retry": True,
+        "retries": 1,
+        "retry_delay": timedelta(minutes=1),
+        },
+    description='Just another trial',
+    start_date = datetime(2023,3,17),
+    schedule = None,
+) as dag:
+    update_urls_task = PythonOperator(
+       task_id= 'update_daily_urls',
+        python_callable = update_past_auction_urls,
+        templates_dict={
+            'urls_file_path':"{{ds}}.txt"
+        }
+    )
+    
+    scrape_daily_urls_task = PythonOperator(
+        task_id='scrape_daily_urls',
+        python_callable = daily_scraper,
+        templates_dict = {
+            'auctions_file_path':'daily auctions/{{ds}}.json',
+            'urls_file_path':'daily urls/{{ds}}.txt'
             
-        with open (f"daily auctions/{datetime.today().date()}.json", 'w') as file:
-            json.dump(auction_data, file, indent=4)
-        
-    def teardown(self):
-        self.driver.quit()
-                 
-scraper = CarScraper("https://carsandbids.com/")
+        }
+    )
+    
+update_urls_task >> scrape_daily_urls_task
 
-# scraper.get_past_auctions_urls()
-scraper.update_past_auction_urls()
-# scraper.scrape_dump_in_chunks(500)
-# scraper.daily_scraper()
-
-scraper.driver.quit() 
+driver.quit()
