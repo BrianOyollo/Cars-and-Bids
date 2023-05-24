0e05c5c56e0a3ae9763ed990bdb8b0d25480aef3
BrianOyollo <77955625+BrianOyollo@users.noreply.github.com>
2023-05-24 12:07:18 +0300

    Delete jobs.sh

--- jobs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /home/brian_oyollo/Documents/projects/demuro/Cars-and-Bids/
-source env/bin/activate
-
-python /home/brian_oyollo/Documents/projects/demuro/Cars-and-Bids/listing.py
-python /home/brian_oyollo/Documents/projects/demuro/Cars-and-Bids/etl.py
-python /home/brian_oyollo/Documents/projects/demuro/Cars-and-Bids/upload.py
