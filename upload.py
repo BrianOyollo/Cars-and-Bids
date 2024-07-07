3ff37cc7f671e0ea84f99641a16b7b0e720262d4
oyollobrian <oyollobrian@gmail.com>
2024-07-07 13:49:58 +0300

    set env variables

--- upload.py
+++ upload.py
@@ -5,8 +5,8 @@ import json
 
 
 # access keys
-aws_access_key_id = os.environ['ACCESS_KEY_ID']
-aws_secret_access_key = os.environ['SECRET_ACCESS_KEY']
+aws_access_key_id = os.environ['AWS_ACCESS_KEY_ID12']
+aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY12']
 
 
 
